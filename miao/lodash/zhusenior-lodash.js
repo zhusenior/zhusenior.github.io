1db80139e0594f89fc1d1a7b3eedca3eace6cd13
seniorzhu <wsy5138@163.com>
2020-12-10 15:16:05 +0800

    new

--- miao/lodash/zhusenior-lodash.js
+++ miao/lodash/zhusenior-lodash.js
@@ -1,80 +1,88 @@
 var zhusenior = {
-  chunk : function(array, [size = 1]) {
-      if (!array)  return null;
-  },
+    chunk: function(array, [size = 1]) {
+        if (!array) return null;
+    },
 
-   compact : function (array) {
-      if (!array) return [];
-      let res = [];
-      for (let ary of array) {
-        if (ary) {
-          res.push(ary);
+    compact: function(array) {
+        if (!array) return [];
+        var res = [];
+        for (var ary of array) {
+            if (ary) {
+                res.push(ary);
+            }
         }
-      }
-      return res;
+        return res;
     },
 
-  difference : function(array, ...values) {
-      if (array == []) return [];
-      let res = [];
-      let value = [];
-      for (let i = 0; i < values.length; i++) {
-        value.push(...values[i]);
-      }
-      for (let ary of array) {
-        let flag = true;
-        for (let val of value) {
-          if (ary == val) {
-            flag = false;
-          }
+    difference: function(array, ...values) {
+        if (array == []) return [];
+        var res = [];
+        var value = [];
+        for (var i = 0; i < values.length; i++) {
+            value.push(...values[i]);
         }
-        if (flag) {
-          res.push(ary);
+        for (var ary of array) {
+            var flag = true;
+            for (var val of value) {
+                if (ary == val) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                res.push(ary);
+            }
         }
-      }
-      return res;
+        return res;
     },
 
-     drop : function (array, n = 1) {
-      if (n == 0) return array;
+    drop: function(array, n = 1) {
+        if (n == 0) return array;
 
-      let res = [];
-      for (let i = n; i < array.length; i++) {
-        res.push(array[i]);
-      }
-      return res;
+        var res = [];
+        for (var i = n; i < array.length; i++) {
+            res.push(array[i]);
+        }
+        return res;
     },
 
-      dropRight:  function (array, n = 1) {
-      if (!array) return [];
+    dropRight: function(array, n = 1) {
+        if (!array) return [];
 
-      if (n > array.length + 1) return [];
-      if (n == 0) return array;
-      let res = [];
-      for (let i = 0; i < array.length - n; i++) {
-        res.push(array[i]);
-      }
-      return res;
+        if (n > array.length + 1) return [];
+        if (n == 0) return array;
+        var res = [];
+        for (var i = 0; i < array.length - n; i++) {
+            res.push(array[i]);
+        }
+        return res;
     },
-      fill : function (array, value, start = 0, end = array.length) {
-      let res = [];
-      for (let i = 0; i < start; i++) {
-        res.push(array[i]);
-      }
-      for (let i = start; i < end; i++) {
-        res.push(value);
-      }
-      for (let i = end; i < array.length; i++) {
-        res.push(array[i]);
-      }
-      return res;
+    fill: function(array, value, start = 0, end = array.length) {
+        var res = [];
+        for (var i = 0; i < start; i++) {
+            res.push(array[i]);
+        }
+        for (var i = start; i < end; i++) {
+            res.push(value);
+        }
+        for (var i = end; i < array.length; i++) {
+            res.push(array[i]);
+        }
+        return res;
     },
-   
-    dropWhile :function(){
+    flatten: function(arr) {
+        var res = [];
+        for (var item of arr) {
+            if (Array.isArray(item)) {
+                res.push(...item);
+            } else {
+                res.push(item);
+            }
+        }
+        return res;
+    },
+
+    dropWhile: function() {
 
     }
 
 }
-
-
-
